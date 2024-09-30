95794e4b5d6077da95bc113e6e398256a5624cc5
MinhHieu <minhieuu1@gmail.com>
2024-09-30 22:57:09 +0700

    Password Encoder

--- src/main/java/com/bidkoi/auctionkoi/AuctionKoiApplication.java
+++ src/main/java/com/bidkoi/auctionkoi/AuctionKoiApplication.java
@@ -2,8 +2,9 @@ package com.bidkoi.auctionkoi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class AuctionKoiApplication {
 
     public static void main(String[] args) {
