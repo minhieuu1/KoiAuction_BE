902a53d42d7f5e0c2ae35317ee74912e1452c992
ThanhAmery <dct01112004@gmail.com>
2024-10-03 11:44:50 +0700

    View profile

--- src/main/java/com/bidkoi/auctionkoi/service/IAccountService.java
+++ src/main/java/com/bidkoi/auctionkoi/service/IAccountService.java
@@ -5,9 +5,17 @@ import com.bidkoi.auctionkoi.dto.AccountDTO;
 import com.bidkoi.auctionkoi.payload.request.AccountCreationRequest;
 import com.bidkoi.auctionkoi.payload.request.LoginRequest;
 import com.bidkoi.auctionkoi.payload.response.LoginResponse;
+import com.bidkoi.auctionkoi.pojo.Account;
+import com.bidkoi.auctionkoi.pojo.Bidder;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface IAccountService {
    AccountDTO createAccount(AccountCreationRequest request);
    LoginResponse login(LoginRequest request);
+   List<Bidder> getAll();
+   Optional<Account> getAccountById(String id);
 
+   Optional<Bidder> getBidderById(String accountId);
 }
