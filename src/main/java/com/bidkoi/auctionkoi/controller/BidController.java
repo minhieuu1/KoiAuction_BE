a6dfd0d92ed2098b20e69deb3eea0e41162b5d41
MinhHieu <minhieuu1@gmail.com>
2024-10-10 11:36:39 +0700

    Fixed Price Bidding

--- src/main/java/com/bidkoi/auctionkoi/controller/BidController.java
+++ src/main/java/com/bidkoi/auctionkoi/controller/BidController.java
@@ -2,6 +2,7 @@ package com.bidkoi.auctionkoi.controller;
 
 import com.bidkoi.auctionkoi.payload.response.ApiResponse;
 import com.bidkoi.auctionkoi.pojo.Bid;
+import com.bidkoi.auctionkoi.pojo.Bidder;
 import com.bidkoi.auctionkoi.service.IBidService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,13 @@ public class BidController {
     ApiResponse<Bid> create(@PathVariable String bidderId, @PathVariable Long roomId) {
         return ApiResponse.<Bid>builder()
                 .data(service.createBid(bidderId,roomId))
+                .build();
+    }
+
+    @GetMapping("/random/{roomId}")
+    ApiResponse<Bidder> random(@PathVariable Long roomId) {
+        return ApiResponse.<Bidder>builder()
+                .data(service.pickRandomBidder(roomId))
                 .build();
     }
 }
--- src/main/java/com/bidkoi/auctionkoi/service/IBidService.java
+++ src/main/java/com/bidkoi/auctionkoi/service/IBidService.java
@@ -1,8 +1,12 @@
 package com.bidkoi.auctionkoi.service;
 
+import com.bidkoi.auctionkoi.dto.BidderDTO;
 import com.bidkoi.auctionkoi.pojo.Bid;
+import com.bidkoi.auctionkoi.pojo.Bidder;
+
 import java.util.List;
 
 public interface IBidService {
-    Bid createBid(String bidderID, Long RoomID);
+    Bid createBid(String bidderID, Long roomID);
+    Bidder pickRandomBidder(Long roomID);
 }
