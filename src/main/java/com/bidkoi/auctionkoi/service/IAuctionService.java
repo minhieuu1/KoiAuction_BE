9d259ca3bb213978044206caa2b1f7f4cd265db2
ThanhAmery <dct01112004@gmail.com>
2024-10-20 23:21:34 +0700

    update auction

--- src/main/java/com/bidkoi/auctionkoi/service/IAuctionService.java
+++ src/main/java/com/bidkoi/auctionkoi/service/IAuctionService.java
@@ -2,6 +2,7 @@ package com.bidkoi.auctionkoi.service;
 
 import com.bidkoi.auctionkoi.dto.AuctionDTO;
 import com.bidkoi.auctionkoi.dto.RoomDTO;
+import com.bidkoi.auctionkoi.payload.request.UpdateAuctionRequest;
 import com.bidkoi.auctionkoi.pojo.Auction;
 
 import java.util.List;
@@ -11,4 +12,11 @@ public interface IAuctionService {
     public RoomDTO addRoomToAuction(Long auctionId, Long roomId);
 
     public List<Auction> getAll();
+
+    public AuctionDTO updateAuction(Long auctionId, UpdateAuctionRequest request);
+
+    public void deleteAuction(Long auctionId);
+
+    AuctionDTO updateStatus(Long auctionId);
+    AuctionDTO getAuctionActive();
 }
