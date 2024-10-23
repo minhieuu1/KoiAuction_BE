1bb36ab4b8882f3267584aa1a9e99fed215301f5
MinhHieu <minhieuu1@gmail.com>
2024-10-23 12:57:40 +0700

    confirm shipping

--- src/main/java/com/bidkoi/auctionkoi/repository/IShippingRepository.java
+++ src/main/java/com/bidkoi/auctionkoi/repository/IShippingRepository.java
@@ -2,11 +2,20 @@ package com.bidkoi.auctionkoi.repository;
 
 import com.bidkoi.auctionkoi.pojo.Bidder;
 import com.bidkoi.auctionkoi.pojo.Breeder;
+import com.bidkoi.auctionkoi.pojo.Koi;
 import com.bidkoi.auctionkoi.pojo.Shipping;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface IShippingRepository extends JpaRepository<Shipping, Long> {
-    boolean existsByBreederAndBidder(Breeder breeder, Bidder bidder);
+    boolean existsByKoiAndBidder(Koi koi, Bidder bidder);
+    Shipping findByKoi(Koi koi);
+    boolean existsByKoi(Koi koi);
+    List<Shipping> findByBidder(Bidder bidder);
+    List<Shipping> findByKoiIn(List<Koi> kois);
+    List<Shipping> findByShippingId(Long shipping);
 }
--- src/main/java/com/bidkoi/auctionkoi/service/IShippingService.java
+++ src/main/java/com/bidkoi/auctionkoi/service/IShippingService.java
@@ -1,9 +1,18 @@
 package com.bidkoi.auctionkoi.service;
 
+import com.bidkoi.auctionkoi.payload.request.InformationRequest;
+import com.bidkoi.auctionkoi.pojo.Koi;
 import com.bidkoi.auctionkoi.pojo.Shipping;
 
+import java.util.List;
+
 public interface IShippingService {
-    Shipping createShipping(Long breederId,String bidderId);
+    boolean isExisted(Long koiId);
+    Shipping createShipping(Long breederId,String bidderId,InformationRequest request);
     void confirmByBreeder(Long shippingId,String img);
     void confirmByBidder(Long shippingId,String img);
+    void confirmInfo(InformationRequest request,Long koiId);
+    List<Shipping> getByBreederId(Long breederId);
+    List<Shipping> getByBidderId(String bidderId);
+    List<Shipping> getByShippingId(Long shippingId);
 }
