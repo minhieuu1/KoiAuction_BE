4e6347aa55284b45a59b285c5fd70a67b85e85dc
Công Thành <dct01112004@gmail.com>
2024-11-04 08:10:28 +0700

    write withdraw api

---
+++ src/main/java/com/bidkoi/auctionkoi/repository/IWithdrawRepository.java
@@ -0,0 +1,8 @@
+package com.bidkoi.auctionkoi.repository;
+
+import com.bidkoi.auctionkoi.pojo.Withdraw;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IWithdrawRepository extends JpaRepository<Withdraw, Long> {
+
+}
