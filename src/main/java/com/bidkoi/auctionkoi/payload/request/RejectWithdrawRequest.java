79391c0566f81211f02f50c8ed120302f66b1294
Công Thành <dct01112004@gmail.com>
2024-11-13 15:44:31 +0700

    update and get withdraw

--- src/main/java/com/bidkoi/auctionkoi/payload/request/RejectWithdrawRequest.java
+++ src/main/java/com/bidkoi/auctionkoi/payload/request/RejectWithdrawRequest.java
@@ -3,16 +3,11 @@ package com.bidkoi.auctionkoi.payload.request;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import java.time.LocalDateTime;
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class WithdrawRequest {
-    Double amount;
-    String accountName;
-    String accountNumber;
-    String bankName;
+public class RejectWithdrawRequest {
+    String description;
 }
