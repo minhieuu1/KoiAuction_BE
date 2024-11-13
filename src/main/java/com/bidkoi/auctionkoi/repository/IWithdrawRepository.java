4a9fae1756cfd0bd84fb17308d1e1e1b6d4de784
Công Thành <dct01112004@gmail.com>
2024-11-13 15:44:31 +0700

    update and get withdraw

--- src/main/java/com/bidkoi/auctionkoi/repository/IWithdrawRepository.java
+++ src/main/java/com/bidkoi/auctionkoi/repository/IWithdrawRepository.java
@@ -1,8 +1,12 @@
 package com.bidkoi.auctionkoi.repository;
 
+import com.bidkoi.auctionkoi.pojo.Account;
 import com.bidkoi.auctionkoi.pojo.Withdraw;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IWithdrawRepository extends JpaRepository<Withdraw, Long> {
 
+    Optional<Withdraw> findWithdrawByAccount(Account account);
 }
--- src/main/java/com/bidkoi/auctionkoi/service/IWithdrawService.java
+++ src/main/java/com/bidkoi/auctionkoi/service/IWithdrawService.java
@@ -2,12 +2,22 @@ package com.bidkoi.auctionkoi.service;
 
 
 import com.bidkoi.auctionkoi.dto.WithdrawDTO;
+import com.bidkoi.auctionkoi.payload.request.RejectWithdrawRequest;
 import com.bidkoi.auctionkoi.payload.request.WithdrawRequest;
+import com.bidkoi.auctionkoi.pojo.Withdraw;
+
+import java.util.List;
 
 public interface IWithdrawService {
     WithdrawDTO createWithdraw(WithdrawRequest request, String accountId);
 
     WithdrawDTO approveWithdraw(Long withdrawId, Long staffId);
 
-    WithdrawDTO rejectWithdraw(Long withdrawId, Long staffId);
+    WithdrawDTO rejectWithdraw(Long withdrawId, Long staffId, RejectWithdrawRequest request);
+
+//    WithdrawDTO getWithdrawById(Long withdrawId);
+
+    List<Withdraw> getAllWithdraw();
+
+    WithdrawDTO getWithdrawByAccountId(String accountId);
 }
